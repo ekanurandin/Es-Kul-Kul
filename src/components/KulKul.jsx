bd34b7bce72669fc7b589397e6b4b1540e14e188
ekanurandini1404@gmail.com <ekanurandini1404@gmail.com>
2024-05-02 00:38:38 -0700

    second commit

--- src/components/KulKul.jsx
+++ src/components/KulKul.jsx
@@ -5,12 +5,14 @@ import React from 'react';
 const KulKul = () => {
   return (
     <TouchableOpacity>
-      {/* <View style={styles.box}>
-        <Text style={{color: 'black'}}>icon</Text>
-      </View>
-      <Text>{label}</Text> */}
       <View>
-        <Text style={{color: 'black', textAlign: 'center', paddingTop: 10}}>
+        <Text
+          style={{
+            color: 'black',
+            textAlign: 'center',
+            paddingTop: 10,
+            fontSize: 20,
+          }}>
           Selamat Datang di Es Kul Kul YMM
         </Text>
       </View>
@@ -20,21 +22,5 @@ const KulKul = () => {
 
 export default KulKul;
 
-const styles = StyleSheet.create({
-  // container: {
-  //   borderWidth: 1,
-  //   margin: 5,
-  //   padding: 5,
-  //   width: 60,
-  //   alignItems: 'center',
-  // },
-  // box: {
-  //   borderWidth: 1,
-  //   width: 48,
-  //   height: 48,
-  //   borderRadius: 8,
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  //   color: 'black',
-  // },
-});
+// eslint-disable-next-line no-unused-vars
+const styles = StyleSheet.create({});
--- src/screens/Beranda.jsx
+++ src/screens/Beranda.jsx
@@ -1,24 +1,10 @@
-import {StyleSheet, View, FlatList} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
 import KulKul from '../components/KulKul';
-
-// const Data = [
-//   {id: 1, label: 'menu 1'},
-//   {id: 2, label: 'menu 2'},
-//   {id: 3, label: 'menu 3'},
-//   {id: 4, label: 'menu 4'},
-//   {id: 5, label: 'menu 5'},
-//   {id: 6, label: 'menu 6'},
-// ];
 
 const Beranda = () => {
   return (
     <View>
-      {/* <FlatList
-        data={Data}
-        renderItem={({item}) => <KulKul label={item.label} />}
-        horizontal={true}
-      /> */}
       <KulKul />
     </View>
   );
